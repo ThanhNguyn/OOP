b0cb8baebc4c6720d3cf0b4c96185b7737bb067e
ThanhNguyn <thanhnguyxntester@gmail.com>
2026-03-09 10:53:23 -0400

    Add 3.3

---
+++ Week2/Ex3.3.py
@@ -0,0 +1,30 @@
+def row2():
+    print("+ - - - - + - - - - +")
+    print("|         |         |")
+    print("|         |         |")
+    print("|         |         |")
+    print("|         |         |")
+
+def grid2x2():
+    row2()
+    row2()
+    print("+ - - - - + - - - - +")
+
+
+def row4():
+    print("+ - - - - + - - - - + - - - - + - - - - +")
+    print("|         |         |         |         |")
+    print("|         |         |         |         |")
+    print("|         |         |         |         |")
+    print("|         |         |         |         |")
+
+def grid4x4():
+    row4()
+    row4()
+    row4()
+    row4()
+    print("+ - - - - + - - - - + - - - - + - - - - +")
+
+grid2x2()
+print()
+grid4x4()
